d0c332bb52f77e7d3d419178f543921c2e8a70c8
miftanurfarid <miftanurfarid@gmail.com>
2020-10-06 08:12:28 +0700

    Update show3Dvisual.m

--- code/show3Dvisual.m
+++ code/show3Dvisual.m
@@ -3,7 +3,7 @@ close all;
 clearvars;
 closeCOMserial();
 
-s=serial('COM3','BAUD', 115200);  % Baud rate and COM port
+s = serial('COM3','BAUD', 115200);  % Baud rate and COM port
 fopen(s);
 
 myaxes = axes('xlim', [-2 2], 'ylim', [-2 10], 'zlim', [-1.5 1.5]);
@@ -16,8 +16,8 @@ ylabel('y');
 zlabel('z');
 
 [xcylinder, ycylinder, zcylinder] = cylinder([0.2 0.2]);
-[xcone, ycone, zcone] = cylinder([0.1 0.15]);
-[xsphere, ysphere, zsphere] = sphere();
+[xcone, ycone, zcone]             = cylinder([0.1 0.15]);
+[xsphere, ysphere, zsphere]       = sphere();
 
 h(1) = surface(xcone, ycone, zcone);
 
@@ -25,19 +25,31 @@ combinedobject = hgtransform('parent',myaxes);
 set(h,'parent', combinedobject);
 drawnow;
 
-while 2>1
+% time set
+tt    = 3; % in seconds
+dt    = zeros(1,2);
+tmp   = clock;
+dt(1) = tmp(6);
+
+% init data
+idx = 1;
+data = zeros(10*tt, 3);
+
+while (dt(2) - dt(1)) < tt
     out = fscanf(s);
     %sprintf(out);
     %disp(out);
     if out(1)=='#'
         parsedData = strsplit(out(1:length(out)-2),{',','=','\n'});
         disp(parsedData);
-        disp(180*str2double(parsedData(2)));
+%         disp(180*str2double(parsedData(2)));
         rotation1 = makehgtform('xrotate', 90 * str2double(parsedData(2))*(pi/180));  % 2
         rotation2 = makehgtform('yrotate', -90 * (str2double(parsedData(4)) - 1)*(pi/180));  % 4
         rotation3 = makehgtform('yrotate', 90 * (str2double(parsedData(6)))*(pi/180));  % 6
         set(combinedobject,'matrix',rotation2);
         drawnow;
     end
-    
+    tmp   = clock;
+    dt(2) = tmp(6);
+    idx = idx + 1;
 end
